35cfa5bd6754427d4e09fe7065d929166f1194c8
santhi-radiant <santhi.g@gmail.com>
2022-03-16 05:10:02 +0530

    data

--- Module3/m3_03.php
+++ Module3/m3_03.php
@@ -7,7 +7,25 @@
 <body>
     <h2>Data Types</h2>
 
-    
+<?php
+//string
+$str="Hello";
+$s="10";
+echo $str.$s.'<br>';
+//integer
+$in=50;
+var_dump($in);
+//float
+$flo=35.6;
+var_dump($flo);
+//bool
+$bool=TRUE;
+var_dump($bool);
+//null
+$condition=null;
+var_dump($condition);
+
+?>
     
 </body>
 
