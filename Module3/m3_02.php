425e33a3c0c90b545dd157b6c80b6901412e3065
santhi-radiant <santhi.g@radiantcloud.co>
2022-03-15 17:25:48 +0530

    print command

--- Module3/m3_02.php
+++ Module3/m3_02.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <pre>
+    <h3>Print System command</h3>
 <?php
 echo "Helloworld<br>";
 Echo "Helloworld<br>";
@@ -15,7 +16,31 @@ $x=10;
 print $x;
 $cmd= "ping 127.0.0.1";
 print shell_exec($cmd);
+?>
 
+<h3>Variable and scope of variable</h3>
+<?php
+$var1="Hello World";
+echo "I want to Print".$var1."<br>";
+$a=10;
+$b=20;
+echo $a+$b;
+$firstVariableIs='Welcome';// camel case variable name
+echo '<br>'.$firstVariableIs;
+
+
+$price=50;//Global variable
+function fruitprice()
+{
+    static $n=1;// static variable remember the value last time used or called
+   $price=20;//local variable
+   echo' apple price per kg is:'.$price; 
+   $n++;
+}
+
+
+fruitprice();
+fruitprice();
 ?>
     </pre>
 </body>
