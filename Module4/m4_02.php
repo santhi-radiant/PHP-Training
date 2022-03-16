e90ceba7ee0c5822b499c67de9edd03511275dec
santhi-radiant <santhi.g@gmail.com>
2022-03-16 21:12:00 +0530

    main

--- Module4/m4_02.php
+++ Module4/m4_02.php
@@ -4,5 +4,9 @@ echo 'Date is'.date("y/m/d").'<br>';
 echo 'Date is'.date("y-m-d").'<br>';
 echo 'Date is'.date("h:i:sa").'<br>';
 $d=mktime(10,11,22,4,2,2023);
-echo "created date is ".date("Y-m-d h:i:sa",$d)
+echo "created date is ".date("Y-m-d h:i:sa",$d);
+echo'<br>';
+
+$da=strtotime("+1 day");
+echo date('Y-m-d h:i:sa',$da);
 ?>
