29d5f6e14014302298d5eec9dc63ec3e21fa5b1e
santhi-radiant <santhi.g@gmail.com>
2022-03-16 17:35:11 +0530

    date commit1

---
+++ Module4/m4_02.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'Date is'.date("y/m/d").'<br>';
+echo 'Date is'.date("y-m-d").'<br>';
+echo 'Date is'.date("h:i:sa").'<br>';
+$d=mktime(10,11,22,4,2,2023);
+echo "created date is ".date("Y-m-d h:i:sa",$d)
+?>
