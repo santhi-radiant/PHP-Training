9e6ee0939d439e5e8ed33f1374922fa5b766f4df
santhi-radiant <santhi.g@gmail.com>
2022-03-17 11:40:28 +0530

    file final read

--- Module4/m4_03.php
+++ Module4/m4_03.php
@@ -7,10 +7,17 @@ $myfile=fopen("readfile.txt","r")or die("unable to reach file");
 //echo fread($myfile,filesize("readfile.txt"));
 //echo fgets($myfile);//to read a single line 
 
+//read line by line
 while(!feof($myfile))
 {
     echo fgets($myfile).'<br>';
 }
+//read character by character
+while(!feof($myfile))
+{
+    echo fgetc($myfile).'<br>';
+}
+
 
 fclose($myfile);
 
