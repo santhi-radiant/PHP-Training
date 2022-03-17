de19154026bbfccffbc3579d88959ea3b1ed1c33
santhi-radiant <santhi.g@gmail.com>
2022-03-17 11:36:57 +0530

    file 3rd commit

--- Module4/m4_03.php
+++ Module4/m4_03.php
@@ -1,5 +1,17 @@
 <?php
 
-echo readfile('readfile.txt');
+//echo readfile('readfile.txt');
+//complete file read operations
+
+$myfile=fopen("readfile.txt","r")or die("unable to reach file");
+//echo fread($myfile,filesize("readfile.txt"));
+//echo fgets($myfile);//to read a single line 
+
+while(!feof($myfile))
+{
+    echo fgets($myfile).'<br>';
+}
+
+fclose($myfile);
 
 ?>
